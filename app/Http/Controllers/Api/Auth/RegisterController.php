31b688293c8c748b1eadc16a71f0652ec7674b96
ulises sandoval <ulises.sandoval@uabc.edu.mx>
2018-06-18 13:03:36 -0700

    funcion register terminada (regresa token)

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -2,15 +2,54 @@
 
 namespace App\Http\Controllers\Api\Auth;
 
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Route;
+use Laravel\Passport\Client;
 
 class RegisterController extends Controller
 {
-    //
+    private $client;
 
-    public function register(Request $request){
-        // Validacion de lo que va resivir 
-        dd($request->all());
+    public function __construct()
+    {
+        $this->client = Client::find(2);
+    }
+
+    public function register(Request $request)
+    {
+        // Validacion de lo que va resivir
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6|confirmed'
+        ]);
+        // si no pasa la validacion regresa error 422 con los campos que no pasaron
+
+
+//         aqui creo el nuevo usuario y lo guardo en la variable user
+        $user = User::create([
+            'name' => request('name'),
+            'email' => request('email'),
+            'password' => bcrypt('password')
+        ]);
+
+        // generando parametros para passport
+        $param = [
+            'grant_type' => 'password',
+            'client_id' => $this->client->id,
+            'client_secret' => $this->client->secret,
+            'username' => request('email'),
+            'password' => request('password'),
+            'scope' => '*'
+        ];
+
+        $request->request->add($param);
+
+        $proxy = Request::create('oauth/token','POST');
+
+        return Route::dispatch($proxy);
+
     }
 }
