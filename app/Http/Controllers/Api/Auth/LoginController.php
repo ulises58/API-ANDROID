c9fb4e9d19a8d4a1ff9de70728e285cdc1bd6635
ulises sandoval <ulises.sandoval@uabc.edu.mx>
2018-06-21 09:55:29 -0700

    metodo login terminado

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -4,18 +4,50 @@ namespace App\Http\Controllers\Api\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use Laravel\Passport\Client;
+use Illuminate\Support\Facades\Route;
 class LoginController extends Controller
 {
 
-    public function login(Request $request){
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = Client::find(2);
+    }
+
+    public function login(Request $request)
+    {
+
+        $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+
+        $param = [
+            'grant_type' => 'password',
+            'client_id' => $this->client->id,
+            'client_secret' => $this->client->secret,
+            'username' => request('username'),
+            'password' => request('password'),
+            'scope' => '*'
+        ];
+
+        $request->request->add($param);
+
+        $proxy = Request::create('oauth/token', 'POST');
+
+        return Route::dispatch($proxy);
+    }
+
+    public function refresh(Request $request)
+    {
 
     }
 
-    public function refresh(Request $request){
-
-    }
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
 
     }
 }
